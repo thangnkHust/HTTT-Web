79a45068c46f9ddf1b906d217544f27e2fd9a3b1
NguyetHMM <nguyet332747@gmail.com>
2021-01-04 13:59:55 +0700

    Init project and create Base layout

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-main class="stylemain">
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+.stylemain {
+  background-image: url("./assets/924.jpg");
+  background-size: cover;
+}
+</style>
